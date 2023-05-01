5bf3835a0899340e2fc5a7933a555b0771be91c4
drbeane <37283017+drbeane@users.noreply.github.com>
2023-05-01 11:35:14 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -21,3 +21,61 @@ def animate_episode(env, steps, sleep=0.1):
         if terminated:
             print('Terminated')
             break
+
+            
+def render_mp4(videopath):
+    from base64 import b64encode
+    mp4 = open(videopath, 'rb').read()
+    base64_encoded_mp4 = b64encode(mp4).decode()
+    return f'<video width=400 controls><source src="data:video/mp4;' \
+           f'base64,{base64_encoded_mp4}" type="video/mp4"></video>'
+
+def record_episode(
+    env, policy, steps=250, env_seed=None, action_seed=None, 
+    updates=False, fname='temp'):
+
+    from gym.wrappers import RecordVideo
+    import os
+    import shutil
+    from IPython.display import HTML
+
+    #if os.path.exists('temp_videos'): shutil.rmtree('temp_videos')
+    vid_env = RecordVideo(env, video_folder='temp_videos', name_prefix=fname, new_step_api=True)
+
+    if env_seed is None:
+        state = vid_env.reset()
+    else:
+        state = vid_env.reset(seed=env_seed)
+        
+    if action_seed is not None:
+        vid_env.action_space.seed(action_seed)
+
+    if updates:
+        print(f'{"step":<6}{"action":<8}{"new state":<30}{"reward":<8}{"terminated":<12}{"info":<8}')
+
+
+    for i in range(steps):
+        a = policy(env, state)
+        obs, reward, terminated, truncated, info = vid_env.step(a)
+        s = str(obs)
+
+        if updates:
+            print(f'{i:<6}{a:<8}{s:30}{reward:<8}{str(terminated):<12}{str(info):<8}')
+
+        if terminated or truncated:
+            break
+
+    print(i + 1, 'steps completed.')
+
+    vid_env.close()
+
+    if not os.path.exists('videos'): os.mkdir('videos')
+    shutil.move(f'/content/temp_videos/{fname}-episode-0.mp4', f'/content/videos/{fname}.mp4')
+    shutil.rmtree('temp_videos')
+
+    html = render_mp4_a(f'/content/videos/{fname}.mp4')
+    display(HTML(html))
+
+
+def random_action(env, state):
+    return env.action_space.sample()
